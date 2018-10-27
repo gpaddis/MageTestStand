33b4d84185051b68e421e7cf2c69ec258a47e93f
Gianpiero Addis <gianpiero.addis@gmail.com>
2018-10-28 00:14:08 +0200

    Ignore any phpunit library

--- install.sh
+++ install.sh
@@ -73,7 +73,7 @@ tools/modman deploy-all --force
 if [[ ! -z $INSTALL_DEPENDENCIES && -f $MODULE_DIR/composer.json ]]; then
   echo "Checking module dependencies..."
   # Get the module dependencies without phpunit, composer installer and php version
-  exclude_modules="ecomdev_phpunit|magento-composer-installer|^php\s"
+  exclude_modules="phpunit|magento-composer-installer|^php\s"
   module_list=($(composer show --self -d ${MODULE_DIR} | awk '/requires/{flag=1;next}/^$/{flag=0}flag' | egrep -v "$exclude_modules"))
   if [ ! ${#module_list[@]} -eq 0 ]; then
       echo "Found dependencies: $module_list"
