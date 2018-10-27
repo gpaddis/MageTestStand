1fc3ede6acfedb8ea70accce7963bdc65c05e9ef
Gianpiero Addis <gianpiero.addis@gmail.com>
2018-10-28 00:06:28 +0200

    Echo found dependencies

--- install.sh
+++ install.sh
@@ -76,6 +76,7 @@ if [[ ! -z $INSTALL_DEPENDENCIES && -f $MODULE_DIR/composer.json ]]; then
   exclude_modules="ecomdev_phpunit|magento-composer-installer|^php\s"
   module_list=($(composer show --self -d ${MODULE_DIR} | awk '/requires/{flag=1;next}/^$/{flag=0}flag' | egrep -v "$exclude_modules"))
   if [ ! ${#module_list[@]} -eq 0 ]; then
+      echo "Found dependencies: $module_list"
       composer require ${module_list// /:}
   else echo "No module dependencies found in composer.json."
   fi
