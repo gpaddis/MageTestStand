0d3eda9f7a68c23903c39768553cb164331d90ec
Gianpiero Addis <gianpiero.addis@gmail.com>
2018-10-28 00:04:25 +0200

    Require the full list

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 MODULE_NAME="$1"
 MODULE_DIR=".modman/${MODULE_NAME}"
@@ -75,9 +76,7 @@ if [[ ! -z $INSTALL_DEPENDENCIES && -f $MODULE_DIR/composer.json ]]; then
   exclude_modules="ecomdev_phpunit|magento-composer-installer|^php\s"
   module_list=($(composer show --self -d ${MODULE_DIR} | awk '/requires/{flag=1;next}/^$/{flag=0}flag' | egrep -v "$exclude_modules"))
   if [ ! ${#module_list[@]} -eq 0 ]; then
-    for package in $module_list; do
-      composer require ${package// /:}
-    done
+      composer require ${module_list// /:}
   else echo "No module dependencies found in composer.json."
   fi
 fi
